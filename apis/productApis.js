4356c73b619e33133bec97e4290ce2e8af4eb239
webdevzsk <72684302+webdevsk@users.noreply.github.com>
2023-11-19 19:57:39 +0600

    Added error handler middleware for getProducts api

--- apis/productApis.js
+++ apis/productApis.js
@@ -3,23 +3,29 @@ import { CACHED_PRODUCT_DATA } from "../index.js"
 
 const router = Router()
 
-router.get("/products", async (req, res) => {
+router.get("/products", async (req, res, next) => {
     if (CACHED_PRODUCT_DATA.statusCode === 500) return res.status(500).json("Server error: DummyJson")
+    try {
 
-    const skip = "skip" in req.query ? parseInt(req.query.skip) : 0
-    const limit = "limit" in req.query ? parseInt(req.query.limit) : 30
-    const { total, categories, brands, exclusiveProducts } = CACHED_PRODUCT_DATA
-    const products = CACHED_PRODUCT_DATA.products.slice(skip, limit)
+        const skip = "skip" in req.query ? parseInt(req.query.skip) : 0
+        const limit = "limit" in req.query ? parseInt(req.query.limit) : 30
+        const { total, categories, brands, exclusiveProducts } = CACHED_PRODUCT_DATA
+        const products = CACHED_PRODUCT_DATA.products.slice(skip, limit)
 
-    res.json({
-        products,
-        exclusiveProducts,
-        skip,
-        limit,
-        total,
-        categories,
-        brands
-    })
+        res.json({
+            products,
+            exclusiveProducts,
+            skip,
+            limit,
+            total,
+            categories,
+            brands
+        })
+    } catch (error) {
+        next(error)
+    }
+}, (err, req, res, next) => {
+    res.status(500).json({ message: err?.message })
 })
 
 export default router
